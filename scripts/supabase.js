6f496e1c1d36910cbac11f812825797ba5e47974
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-08-02 22:14:59 +0800

    Update supabase.js

--- scripts/supabase.js
+++ scripts/supabase.js
@@ -1,5 +1,6 @@
 // supabase.js
-const supabase = supabase.createClient(
-  'https://iabclikcfddqjcswhqwo.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhYmNsaWtjZmRkcWpjc3docXdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxMjM4NjIsImV4cCI6MjA2OTY5OTg2Mn0.IpGizEYbKqQUb8muy335lYCeP-u7mrFLJLUQO9oHPkw'
-);
+import { createClient } from 'https://esm.sh/@supabase/supabase-js';
+
+const supabaseUrl = "https://iabclikcfddqjcswhqwo.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhYmNsaWtjZmRkcWpjc3docXdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxMjM4NjIsImV4cCI6MjA2OTY5OTg2Mn0.IpGizEYbKqQUb8muy335lYCeP-u7mrFLJLUQO9oHPkw";
+export const supabase = createClient(supabaseUrl, supabaseKey);
