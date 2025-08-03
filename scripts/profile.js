d7077831b3a16664603ecc5d5ed84a1afd18f8ea
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-08-03 16:33:39 +0800

    Update profile.js

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,6 +1,6 @@
 const supabase = supabase.createClient(
   'https://iabclikcfddqjcswhqwo.supabase.co',
-  'YOUR_SUPABASE_ANON_KEYeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhYmNsaWtjZmRkcWpjc3docXdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxMjM4NjIsImV4cCI6MjA2OTY5OTg2Mn0.IpGizEYbKqQUb8muy335lYCeP-u7mrFLJLUQO9oHPkw'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhYmNsaWtjZmRkcWpjc3docXdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxMjM4NjIsImV4cCI6MjA2OTY5OTg2Mn0.IpGizEYbKqQUb8muy335lYCeP-u7mrFLJLUQO9oHPkw'
 );
 
 // Load profile photo on page load
