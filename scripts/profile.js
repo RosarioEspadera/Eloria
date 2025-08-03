2d6ce3a45898208db0d9c722d52a3ef28b6bf4c2
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-08-03 16:29:45 +0800

    Update profile.js

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,8 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://iabclikcfddqjcswhqwo.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhYmNsaWtjZmRkcWpjc3docXdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxMjM4NjIsImV4cCI6MjA2OTY5OTg2Mn0.IpGizEYbKqQUb8muy335lYCeP-u7mrFLJLUQO9oHPkw';
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = supabase.createClient(
+  'https://iabclikcfddqjcswhqwo.supabase.co',
+  'YOUR_SUPABASE_ANON_KEYeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhYmNsaWtjZmRkcWpjc3docXdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxMjM4NjIsImV4cCI6MjA2OTY5OTg2Mn0.IpGizEYbKqQUb8muy335lYCeP-u7mrFLJLUQO9oHPkw'
+);
 
 // Load profile photo on page load
 (async () => {
