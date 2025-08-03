b6410eba8270a320771e5000bfaca8d490972375
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-08-03 20:19:16 +0800

    Update profile.js

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,4 +1,4 @@
-const supabase = supabase.createClient(
+const supabase = window.supabase.createClient(
   'https://iabclikcfddqjcswhqwo.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhYmNsaWtjZmRkcWpjc3docXdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxMjM4NjIsImV4cCI6MjA2OTY5OTg2Mn0.IpGizEYbKqQUb8muy335lYCeP-u7mrFLJLUQO9oHPkw'
 );
