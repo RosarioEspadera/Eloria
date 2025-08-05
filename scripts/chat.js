ca93ddf88d37ece6e37c0f860d72318a91abab52
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-08-05 20:39:52 +0800

    Update chat.js

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,7 +1,7 @@
  import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = "https://iabclikcfddqjcswhqwo.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhYmNsaWtjZmRkcWpjc3docXdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxMjM4NjIsImV4cCI6MjA2OTY5OTg2Mn0.IpGizEYbKqQUb8muy335lYCeP-u7mrFLJLUQO9oHPkw";
+const SUPABASE_URL = "https://iabclikcfddqjcswhqwo.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhYmNsaWtjZmRkcWpjc3docXdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxMjM4NjIsImV4cCI6MjA2OTY5OTg2Mn0.IpGizEYbKqQUb8muy335lYCeP-u7mrFLJLUQO9oHPkw";
 const ADMIN_ID = '6d8cbbe7-47b5-4398-97f9-d91823b44110';
 let supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
