5cbf0b303b254320964940d590a44c23b019483a
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-08-05 22:49:54 +0800

    Update chat.js

--- scripts/chat.js
+++ scripts/chat.js
@@ -153,8 +153,10 @@ function scrollToBottom() {
 }
 
 function sanitize(str) {
+  if (typeof str !== 'string') return '';
   return str
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;');
 }
+
